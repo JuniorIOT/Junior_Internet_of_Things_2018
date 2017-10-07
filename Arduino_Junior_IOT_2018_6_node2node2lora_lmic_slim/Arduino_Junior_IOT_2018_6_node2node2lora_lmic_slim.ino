40579146270fe3be96abc90fbe001970d1770bc8
Dennis Ruigrok <dennis.ruigrok@gmail.com>
2017-10-07 20:48:27 +0200

    Lora only now about to add radio

---
+++ Arduino_Junior_IOT_2018_6_node2node2lora_lmic_slim/Arduino_Junior_IOT_2018_6_node2node2lora_lmic_slim.ino
@@ -0,0 +1,40 @@
+#include <avr/pgmspace.h>
+
+// LoRa credentials
+#include "keys.h"
+
+#include <lmic_slim.h>
+
+void setup() {
+    delay (5000);
+    spi_start();
+    pinMode(SS_pin, OUTPUT);                                                                  
+    pinMode(SCK_pin, OUTPUT);                                         
+    pinMode(MOSI_pin, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(SCK_pin, LOW);                                                   // SCK low
+    digitalWrite(SS_pin, HIGH);                                                   // NSS high
+    delay(10);
+    writeReg(0x01, 0x08);
+    delay(10);
+    radio_init ();
+    delay(10);
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (DEVADDR, nwkskey, appskey);
+}
+
+void loop() {
+      LMIC_setTxData2(mydata, sizeof(mydata)-1);
+      radio_init ();                                                       
+      delay (10);
+      digitalWrite(LED_BUILTIN, HIGH);
+      txlora ();
+      delay(1000);                                                                 // wacht op TX ready. Airtime voor 5 bytes payload = 13 x 2^(SF-6) ms
+      digitalWrite(LED_BUILTIN, LOW);
+      setopmode(0x00);                                                             // opmode SLEEP
+      delay (60000);                                                               // Wacht 1 minuut
+}
+
