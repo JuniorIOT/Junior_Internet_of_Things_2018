170082a3f103d9e2e45456dc42bfe0de8e1b4e84
Dennis Ruigrok <dennis.ruigrok@gmail.com>
2017-10-31 20:50:14 +0100

    GPS should work

--- sodaq_Explorer_nb-iot/arduino/arduino.ino
+++ sodaq_Explorer_nb-iot/arduino/arduino.ino
@@ -44,7 +44,8 @@ void loop();
 void put_gpsvalues_into_sendbuffer();
 void gps_init();
 void find_fix(uint32_t delay_until);
-
+void doGPS_and_put_values_into_lora_sendbuffer();
+void put_gpsvalues_into_lora_sendbuffer();
 
 //////////////////////////////////////////////////
 // Kaasfabriek routines for RN2483 for LoraWan
@@ -162,8 +163,9 @@ void setup() {
   /*TODO
    * put_Volts_and_Temp_into_sendbuffer();
   put_Compass_and_Btn_into_sendbuffer();
-  doGPS_and_put_values_into_sendbuffer();   
-*/
+  */
+  doGPS_and_put_values_into_lora_sendbuffer();   
+
   DEBUG_STREAM.print(F("\nSend one lorawan message as part of system init. milis=")); DEBUG_STREAM.println(millis());
 
   last_lora_time = millis();
@@ -215,8 +217,9 @@ void loop() {
    * TODO
    * put_Volts_and_Temp_into_sendbuffer();
   put_Compass_and_Btn_into_sendbuffer();
-  doGPS_and_put_values_into_sendbuffer();
   */
+  doGPS_and_put_values_into_lora_sendbuffer();
+  
   
   ////////// Now we need to send a LORAWAN update to the world  ///////////
   // switch the LMIC antenna to LoraWan mode
@@ -232,6 +235,9 @@ void loop() {
 //////////////////////////////////////////////////////////
 //// Kaasfabriek routines for gps
 ////////////////////////////////////////////
+long l_lat, l_lon, l_alt;
+int hdopNumber;
+
 void gps_init() {
   sodaq_gps.init(6);
   #ifdef DEBUG
@@ -248,20 +254,97 @@ void find_fix(uint32_t delay_until)
     uint32_t timeout = delay_until * 1000; // timeout
     DEBUG_STREAM.println(String("waiting for fix ..., timeout=") + timeout + String("ms"));
     if (sodaq_gps.scan(false, timeout)) {
-      /*
-       * String message = "";
-        message +=(String(" time to find fix: ") + (millis() - start) + String("ms"));
-        message +=(String(" datetime = ") + sodaq_gps.getDateTimeString());
-        message +=(String(" lat = ") + String(sodaq_gps.getLat(), 7));
-        message +=(String(" lon = ") + String(sodaq_gps.getLon(), 7));
-        message +=(String(" num sats = ") + String(sodaq_gps.getNumberOfSatellites()));
-        
-       */
-        
+      
+      l_lat = sodaq_gps.getLat();
+      l_lon = sodaq_gps.getLon();
+      l_alt = sodaq_gps.getAlt();
+      hdopNumber = sodaq_gps.getHDOP();
+      
     } else {
         DEBUG_STREAM.println("No Fix");        
     }
 }
+
+void put_gpsvalues_into_lora_sendbuffer() {
+  
+  Serial.println(F("Started: put_gpsvalues_into_sendbuffer"));
+  
+  //   GPS reading = Satellite time hh:mm:18, lat 526326595, lon 47384133, alt 21, hdop 990, sat count = 12
+  // With the larger precision in LONG values in NMEAGPS, 
+  //    when rescaling, the values exceed the range for type LONG  -2.147.483.648 .. 2.147.483.647
+  //    so we need to use DOUBLE with 15 digits precision, not preferred is FLOAT with 7 digits
+  //    our values such as 526326595 have 9 digits
+    
+  const double shift_lat     =    90. * 10000000.;                 // range shift from -90..90 into 0..180, note: 
+                                                                 //      NMEAGPS long lat&lon are degree values * 10.000.000
+                                                                 //      TynyGPS long lat&lon are degree values * 1.000.000
+  const double max_old_lat   =   180. * 10000000.;                 // max value for lat is now 180
+  const double max_3byte     =         16777215.;                   // max value that fits in 3 bytes
+  double lat_DOUBLE         = l_lat;                              // put 4byte LONG into a more precise floating point to prevent rounding during calcs 
+  lat_DOUBLE = (lat_DOUBLE + shift_lat) * max_3byte / max_old_lat; // rescale into 3 byte integer range
+  uint32_t LatitudeBinary  = lat_DOUBLE;                          // clips off anything after the decimal point    
+  const double shift_lon     =   180. * 10000000.;                 // range shift from -180..180 into 0..360
+  const double max_old_lon   = 360. * 10000000.;                   // max value longitude is now 360, note the value is too big for Long type
+  double lon_DOUBLE = l_lon;                                      // put the 4byte LONG into a precise floating point memory space
+  lon_DOUBLE = (lon_DOUBLE + shift_lon) * max_3byte / max_old_lon; // rescale into 3 byte integer range
+  uint32_t LongitudeBinary = lon_DOUBLE;                          // clips off anything after the decimal point  
+  uint16_t altitudeBinary  = l_alt    ;                          // we want altitudeGps in meters, note:
+                                                                 //      NMEAGPS alt.whole is meter value 
+                                                                 //      TynyGPS long alt is meter value * 100
+  if (l_alt<0) altitudeBinary=0;                                 // unsigned int wil not allow negative values and warps them to huge number  
+  uint8_t HdopBinary = hdopNumber/100;                           // we want horizontal dillution, good is 2..5, poor is >20. Note:
+                                                                 //      NMEAGPS outputs an indoor value of 600..1000. Let's divide by 100
+                                                                 //      from TinyGPS horizontal dilution of precision in 100ths? We succesfully divided by 10
+                                                                 //      TinyGPSplus seems the same in 100ths as per MNEMA string. We succesfully divided by 10
+  
+//  Serial.print(F("  shift_lat = "));//Serial.println(shift_lat);
+//  Serial.print(F("  max_old_lat = "));//Serial.println(max_old_lat);
+//  Serial.print(F("  max_3byte = "));//Serial.println(max_3byte);
+//  Serial.print(F("  l_lat = "));//Serial.println(l_lat);
+//  Serial.print(F("  lat_float = "));//Serial.println(lat_float);
+//  Serial.print(F("  LatitudeBinary = "));//Serial.println(LatitudeBinary);
+//  
+//  Serial.print(F("\n  shift_lon = "));//Serial.println(shift_lon);
+//  Serial.print(F("  max_old_lon = "));//Serial.println(max_old_lon);
+//  Serial.print(F("  l_lon = "));//Serial.println(l_lon);
+//  Serial.print(F("  lon_float = "));//Serial.println(lon_float);
+//  Serial.print(F("  LongitudeBinary = "));//Serial.println(LongitudeBinary);
+//  
+//  Serial.print(F("\n  l_alt = "));//Serial.println(l_alt);
+//  Serial.print(F("  altitudeBinary = "));//Serial.println(altitudeBinary);
+//  
+//  //Serial.print(F("\n  hdopNumber = "));//Serial.println(hdopNumber);
+//  //Serial.print(F("  HdopBinary = "));//Serial.println(HdopBinary);
+  
+  myLoraWanData[0] = ( LatitudeBinary >> 16 ) & 0xFF;
+  myLoraWanData[1] = ( LatitudeBinary >> 8 ) & 0xFF;
+  myLoraWanData[2] = LatitudeBinary & 0xFF;
+  myLoraWanData[3] = ( LongitudeBinary >> 16 ) & 0xFF;
+  myLoraWanData[4] = ( LongitudeBinary >> 8 ) & 0xFF;
+  myLoraWanData[5] = LongitudeBinary & 0xFF;
+  // altitudeGps in meters into unsigned int
+  myLoraWanData[6] = ( altitudeBinary >> 8 ) & 0xFF;
+  myLoraWanData[7] = altitudeBinary & 0xFF;
+  // hdop in tenths of meter
+  myLoraWanData[8] = HdopBinary & 0xFF;
+
+  print_myLoraWanData();
+  
+//  Dummy satellite values: time hh:mm:5, lat 52632400, lon 4738800, alt 678, hdop 2345, sat count = 12
+//       0  1  2  3  4  5  6  7  8   
+//     [87 7C 49 80 56 44 02 A6 17 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  .. ]
+
+}
+
+void doGPS_and_put_values_into_lora_sendbuffer() {
+  find_fix(2); // find fix in 2 seconds
+  // put gps values into send buffer
+  put_gpsvalues_into_lora_sendbuffer();
+  
+  Serial.print(F("\Completed: doGPS_and_put_values_into_sendbuffer. milis=")); Serial.println(millis());
+  
+}
+
 
 
 // Leds
