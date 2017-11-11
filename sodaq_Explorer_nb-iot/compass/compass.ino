a933b42c7c8157502b2c1806dac6d73f8a70826c
Dennis Ruigrok <dennis.ruigrok@gmail.com>
2017-11-11 23:00:49 +0100

    Compass guass checked with self test - works. Degrees still strange behaviour

--- sodaq_Explorer_nb-iot/compass/compass.ino
+++ sodaq_Explorer_nb-iot/compass/compass.ino
@@ -16,9 +16,15 @@ void setup()
 {
   SerialUSB.begin(9600);
   Wire.begin(); // join i2c bus (address optional for master)
+
+  
+
+
 }
 
 byte x = 0;
+
+
 
 void loop()
 {
@@ -36,8 +42,8 @@ Wire.endTransmission();
 SerialUSB.println("Writing [temp on] [xy low power] [xy low power] [10hz] [10hz] [10h] [self test off] [0]");
 SerialUSB.println("0b10010000");
 SerialUSB.println("To address 0x20 CTRL_REG1_M");
-//[temp on] [xy low power] [xy low power] [10hz] [10hz] [10h] [self test off] [0] 
-set_register(0x20, 0b10010000); 
+//[temp off] [xy low power] [xy low power] [10hz] [10hz] [10h] [self test off] [0] 
+set_register(0x20, 0b00010000); 
 
 Wire.beginTransmission(compass_device);    // Get the slave's attention, tell it we're sending a command byte
 Wire.write(0x20);                               //  The command byte, sets pointer to register with address of 0x32
@@ -51,10 +57,10 @@ slaveByte2 = Wire.read();        // read that byte into 'slaveByte2' variable
 
 SerialUSB.println("Writing to address 0x21 CTRL_REG2_M");
 // CTRL_REG2_M
-// [always 0] [standard resolution] [standard resolution] [always 0] [do not reboot memory] [do not soft reset] [always 0] [always 0]
-set_register(0x21, 0b00000000); 
-SerialUSB.println("[always 0] [standard resolution] [standard resolution] [always 0] [do not reboot memory] [do not soft reset] [always 0] [always 0]");
-SerialUSB.println("0b00000000");
+// [always 0] [16 guass full scale used] [16 guass full scale used] [always 0] [do not reboot memory] [do not soft reset] [always 0] [always 0]
+set_register(0x21, 0b01100000); 
+SerialUSB.println("[always 0] [16 guass full scale used] [16 guass full scale used] [always 0] [do not reboot memory] [do not soft reset] [always 0] [always 0]");
+SerialUSB.println("0b01100000");
 ;
 
 Wire.beginTransmission(compass_device);    // Get the slave's attention, tell it we're sending a command byte
@@ -69,10 +75,10 @@ slaveByte2 = Wire.read();        // read that byte into 'slaveByte2' variable
 
 //CTRL_REG3_M
 SerialUSB.println("Writing to address 0x22 CTRL_REG3_M");
-SerialUSB.println("[i2c enable] [always 0] [low power] [always 0] [always 0][spi only write - but not using this] [Continuous-conversion mode] [Continuous-conversion mode]");
+SerialUSB.println("[i2c enable] [always 0] [low power] [always 0] [always 0][spi only write - but not using this] [Single conversion] [Single conversion]");
 SerialUSB.println("0b00000000");
-// doubting [Continuous-conversion mode]
-set_register(0x22, 0b00000000);
+
+set_register(0x22, 0b00000001);
 ;
 Wire.beginTransmission(compass_device);    // Get the slave's attention, tell it we're sending a command byte
 Wire.write(0x22);                               //  The command byte, sets pointer to register with address of 0x32
@@ -117,7 +123,6 @@ slaveByte2 = Wire.read();        // read that byte into 'slaveByte2' variable
 
 
 
-
 // OUT_X_L_M 28
 // OUT_X_H_M 29
 Wire.beginTransmission(compass_device);    // Get the slave's attention, tell it we're sending a command byte
@@ -156,7 +161,7 @@ Wire.write(0x2D);                               //  The command byte, sets point
 Wire.endTransmission();
 Wire.requestFrom(compass_device,1);          // Tell slave we need to read 1byte from the current register
 byte OUT_Z_H_M = Wire.read();        // read that byte into 'slaveByte2' variable
-
+/*
 // TEMP_L_M 2E
 // TEMP_H_M 2F
 Wire.beginTransmission(compass_device);    // Get the slave's attention, tell it we're sending a command byte
@@ -169,16 +174,16 @@ Wire.write(0x2F);                               //  The command byte, sets point
 Wire.endTransmission();
 Wire.requestFrom(compass_device,1);          // Tell slave we need to read 1byte from the current register
 byte TEMP_H_M = Wire.read();        // read that byte into 'slaveByte2' variable
-
+*/
 SerialUSB.print("OUT_X_H_M: ");
 SerialUSB.print(OUT_X_H_M,HEX);
 SerialUSB.print("OUT_X_L_M: ");
 SerialUSB.println(OUT_X_L_M,HEX);
 
-int16_t x = ((OUT_X_H_M << 8) + OUT_X_L_M);
-int16_t y = ((OUT_Y_H_M << 8) + OUT_Y_L_M);
-int16_t z = ((OUT_Z_H_M << 8) + OUT_Z_L_M);
-int16_t temp = ((TEMP_H_M << 8) + TEMP_L_M);
+int16_t x = ((OUT_X_H_M << 8) | OUT_X_L_M);
+int16_t y = ((OUT_Y_H_M << 8) | OUT_Y_L_M);
+int16_t z = ((OUT_Z_H_M << 8) | OUT_Z_L_M);
+//int16_t temp = ((TEMP_H_M << 8) | TEMP_L_M);
 
 SerialUSB.print("x");
 SerialUSB.println(x,DEC);
@@ -189,28 +194,32 @@ SerialUSB.println(z, DEC);
 
 
 
-float X_milliGauss = x * 0.58;
-float Y_milliGauss = y * 0.58;
-float Z_milliGauss = z * 0.58;
+float X_Gauss = ((float)x) * 0.58/1000;
+float Y_Gauss = ((float)y) * 0.58/1000;
+float Z_Gauss = ((float)z) * 0.58/1000;
 
-SerialUSB.print("x mgauss");
-SerialUSB.println(X_milliGauss,DEC);
-SerialUSB.print("y: mgauss");
-SerialUSB.println(Y_milliGauss, DEC);
-SerialUSB.print("z: mgauss");
-SerialUSB.println(Z_milliGauss, DEC);
+SerialUSB.println("0.25–0.60 gauss – the Earth's magnetic field at its surface");
+SerialUSB.print("x gauss");
+SerialUSB.println(X_Gauss,DEC);
+SerialUSB.print("y: gauss");
+SerialUSB.println(Y_Gauss, DEC);
 
+float headingDegrees;
 
-
-float heading, headingDegrees, headingFiltered, geo_magnetic_declination_deg;
+float geo_magnetic_declination_deg;
 geo_magnetic_declination_deg = 1.09; // for our location
-  
-  //Calculating Heading
-  headingDegrees = atan2(Y_milliGauss, X_milliGauss)* 180/PI + geo_magnetic_declination_deg;  // heading in rad. 
+SerialUSB.print("180/PI=");
+SerialUSB.println(180/PI);
+SerialUSB.print("atan y/z=");
+SerialUSB.println(atan2(Y_Gauss, X_Gauss));
+
+  headingDegrees = atan2(Y_Gauss, X_Gauss) * 180/PI + geo_magnetic_declination_deg;  // heading in rad. 
   
   // Correcting when signs are reveresed or due to the addition of the geo_magnetic_declination_deg angle
   if(headingDegrees <0) headingDegrees += 2*180;
 if(headingDegrees > 2*180) headingDegrees -= 2*180;
+
+
 SerialUSB.print("heading: ");
 SerialUSB.println(headingDegrees);
 
