2aa67ed8600ce0cd402bf5c369234186cd1c338f
Marco van Schagen <marco@marcovanschagen.nl>
2018-03-14 22:36:49 +0100

    completed lora32u4 lib list

--- juniorIOTchallenge_Lora32u4_gpsTracker_with_extras/juniorIOTchallenge_Lora32u4_gpsTracker_with_extras.ino
+++ juniorIOTchallenge_Lora32u4_gpsTracker_with_extras/juniorIOTchallenge_Lora32u4_gpsTracker_with_extras.ino
@@ -12,9 +12,6 @@
 //    <NMEAGPS.h>      --> C:\arduino_port\GitHub\Junior_Internet_of_Things_2018\libraries\NeoGPS-master.zip
 //    <lmic_slim.h>    --> C:\arduino_port\GitHub\Junior_Internet_of_Things_2018\libraries\lmic_slim.zip
 //    <RH_RF95.h>      --> C:\arduino_port\GitHub\Junior_Internet_of_Things_2018\libraries\RadioHead-1.79.zip
-//         --> C:\arduino_port\GitHub\Junior_Internet_of_Things_2018\libraries\
-//         --> C:\arduino_port\GitHub\Junior_Internet_of_Things_2018\libraries\
-//         --> C:\arduino_port\GitHub\Junior_Internet_of_Things_2018\libraries\
  
 //#define DEBUG           // if DEBUG is defined, some code is added to display some basic debug info
 //#define DEBUGLEVEL2     // if DEBUGLEVEL2 is defined, some code is added to display deeper debug info
